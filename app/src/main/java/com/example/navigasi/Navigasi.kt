127fbb04a88ed57441bf8baabde4fa56843bb929
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-11-06 09:30:22 +0700

    mendefinisikan rute navigasi dengan enum class

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,2 +1,15 @@
 package com.example.navigasi
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
